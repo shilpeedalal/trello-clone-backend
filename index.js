e0384976940c76aac5d52e3777faf51136840f7d
shilpeedalal <105790624+shilpeedalal@users.noreply.github.com>
2024-08-23 00:25:28 +0530

    User login and register APIs completed

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const express = require("express")
+const express = require('express')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+const authRoutes = require('./routes/authRoutes')
+
+dotenv.config()
 
 const app = express()
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
+app.use(express.json())
+
+connectDB()
+
+app.use('/api/auth', authRoutes)
 
 app.get("/", (req,res)=>{
     res.send("Hello world")
