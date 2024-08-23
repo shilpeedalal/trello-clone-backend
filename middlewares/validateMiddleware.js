18756ff037cb05c1ac786ba7420d00849f7bb328
shilpeedalal <shilpeedalal937@gmail.com>
2024-08-23 11:43:53 +0530

    error fixed for the validation

--- middlewares/validateMiddleware.js
+++ middlewares/validateMiddleware.js
@@ -1,25 +1,27 @@
-const joi = require('joi')
+const Joi = require('joi');
 
 const userSchema = Joi.object({
-    email: Joi.string().email.required().message({
+    email: Joi.string().email().required().messages({
         'string.email': 'Please provide a valid email address',
         'any.required': 'Email is required'
     }),
-    password: Joi.string().pattern(new RegExp('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,}$'))
-    .required().message({
+    password: Joi.string().pattern(new RegExp('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,}$')).required().messages({
         'string.pattern.base': 'Password must be at least 6 characters long and contain at least one number and one letter',
         'any.required': 'Password is required'
     })
-})
+});
 
 const validateUser = (req, res, next) => {
-    const {error} = userSchema.validate(req.body, {abortEarly: false})
-    if(error){
+    const { error } = userSchema.validate(req.body, { abortEarly: false });
+    
+    if (error) {
+        const errorMessages = error.details.map(err => err.message);
+
         return res.status(400).json({
-            errors: error.details.map(err => err.message) 
-        })
+            errors: errorMessages
+        });
     }
-    next()
+    next();
 }
 
-module.exports = validateUser
+module.exports = validateUser;
--- models/User.js
+++ models/User.js
@@ -1,21 +1,31 @@
-const mongoose = require("mongoose")
-const bcrypt = require('bcryptjs')
+const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: [true, 'Name is required']
     },
     email: {
         type: String,
-        required: true,
-        unique: true
+        required: [true, 'Email is required'],
+        unique: true,
+        match: [
+            /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+            'Please provide a valid email address'
+        ],
+        index: true
     },
     password: {
         type: String,
-        required: true,
+        required: [true, 'Password is required'],
+        minlength: [6, 'Password must be at least 6 characters long'],
+        match: [
+            /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/,
+            'Password must contain at least one letter and one number'
+        ]
     }
-})
+}, { timestamps: true });
 
 userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next();
@@ -24,9 +34,18 @@ userSchema.pre('save', async function (next) {
     next();
 });
 
+
 userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
-const User = mongoose.model('User', userSchema)
-module.exports = User
+userSchema.post('save', function(error, doc, next) {
+    if (error.name === 'MongoServerError' && error.code === 11000) {
+        next(new Error('Email already exists, please use a different email.'));
+    } else {
+        next(error);
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
